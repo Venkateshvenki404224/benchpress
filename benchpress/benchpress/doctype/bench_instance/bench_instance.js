3e9630b2ce57f429d9da0b5fad57a1af92ed64bf
M Venkatesh <venkateshvenki404224@gmail.com>
2026-03-19 19:23:23 +0000

    feat: add vpn config download and copy buttons to bench form
    Download VPN Config creates a .conf file blob download.
    Copy VPN Config copies the WireGuard config to clipboard.
    Both buttons appear under VPN group when config is available.

--- benchpress/benchpress/doctype/bench_instance/bench_instance.js
+++ benchpress/benchpress/doctype/bench_instance/bench_instance.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2026, Venkatesh and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Bench Instance", {
-// 	refresh(frm) {
+frappe.ui.form.on("Bench Instance", {
+	refresh(frm) {
+		if (frm.doc.wg_config && frm.doc.status === "Running") {
+			frm.add_custom_button(__("Download VPN Config"), () => {
+				const blob = new Blob([frm.doc.wg_config], { type: "text/plain" });
+				const url = URL.createObjectURL(blob);
+				const a = document.createElement("a");
+				a.href = url;
+				a.download = `${frm.doc.bench_name || frm.doc.name}.conf`;
+				a.click();
+				URL.revokeObjectURL(url);
+			}, __("VPN"));
 
-// 	},
-// });
+			frm.add_custom_button(__("Copy VPN Config"), () => {
+				navigator.clipboard.writeText(frm.doc.wg_config).then(() => {
+					frappe.show_alert({
+						message: __("WireGuard config copied to clipboard"),
+						indicator: "green",
+					});
+				});
+			}, __("VPN"));
+		}
+	},
+});
